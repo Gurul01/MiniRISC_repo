3231c9e8172c55f7e40e7b3896040b922981d117
Gurul01 <nedbalek.ors@gmail.com>
2024-09-18 12:50:09 +0200

    Zero accumulation added
    Didn't need zero_acc flag. It is enough to accumulate the previous value of the zero flag when there is a SBC (this alu operation is also the one that gets executed when doing a CMY)

--- MiniRISC_CPU/alu.v
+++ MiniRISC_CPU/alu.v
@@ -3,30 +3,30 @@
 //******************************************************************************
 //* MiniRISC CPU v2.0                                                          *
 //*                                                                            *
-//* Aritmetikai-logikai egység (ALU). Támogatott mûveletek:                    *
-//* 1) Adatmozgatás (nincs mûveletvégzés)                                      *
-//* 2) Összeadés és kivonás átvitel nélkül, illetve átvitellel                 *
-//* 3) Bitenkénti ÉS, VAGY és XOR                                              *
-//* 4) Shiftelés és forgatás                                                   *
+//* Aritmetikai-logikai egysï¿½g (ALU). Tï¿½mogatott mï¿½veletek:                    *
+//* 1) Adatmozgatï¿½s (nincs mï¿½veletvï¿½gzï¿½s)                                      *
+//* 2) ï¿½sszeadï¿½s ï¿½s kivonï¿½s ï¿½tvitel nï¿½lkï¿½l, illetve ï¿½tvitellel                 *
+//* 3) Bitenkï¿½nti ï¿½S, VAGY ï¿½s XOR                                              *
+//* 4) Shiftelï¿½s ï¿½s forgatï¿½s                                                   *
 //******************************************************************************
 module alu(
-   //Órajel.
+   //ï¿½rajel.
    input  wire       clk,
    
-   //Vezérlõ bemenetek.
-   input  wire [1:0] op_type,       //Az ALU mûvelet típusát kiválasztó jel
-   input  wire [1:0] arith_sel,     //Az aritmetikai mûveletet kiválasztó jel
-   input  wire [1:0] logic_sel,     //A logikai mûveletet kiválasztó jel
-   input  wire [3:0] shift_sel,     //A shiftelési mûveletet kiválasztó jel
+   //Vezï¿½rlï¿½ bemenetek.
+   input  wire [1:0] op_type,       //Az ALU mï¿½velet tï¿½pusï¿½t kivï¿½lasztï¿½ jel
+   input  wire [1:0] arith_sel,     //Az aritmetikai mï¿½veletet kivï¿½lasztï¿½ jel
+   input  wire [1:0] logic_sel,     //A logikai mï¿½veletet kivï¿½lasztï¿½ jel
+   input  wire [3:0] shift_sel,     //A shiftelï¿½si mï¿½veletet kivï¿½lasztï¿½ jel
    
-   //Az operandusok és az eredmény.
-   input  wire [7:0] operand1,      //Elsõ operandus
-   input  wire [7:0] operand2,      //Második operandus
-   output wire [7:0] result,        //Az ALU mûvelet eredménye
+   //Az operandusok ï¿½s az eredmï¿½ny.
+   input  wire [7:0] operand1,      //Elsï¿½ operandus
+   input  wire [7:0] operand2,      //Mï¿½sodik operandus
+   output wire [7:0] result,        //Az ALU mï¿½velet eredmï¿½nye
    
-   //ALU feltétel jelek.
-   input  wire [3:0] flag_din,      //A flag-ekbe írandó érték
-   input  wire       flag_wr,       //A flag-ek írás engedélyezõ jele
+   //ALU feltï¿½tel jelek.
+   input  wire [3:0] flag_din,      //A flag-ekbe ï¿½randï¿½ ï¿½rtï¿½k
+   input  wire       flag_wr,       //A flag-ek ï¿½rï¿½s engedï¿½lyezï¿½ jele
    output reg        flag_z,        //Zero flag
    output reg        flag_c,        //Carry flag
    output reg        flag_n,        //Negative flag
@@ -37,133 +37,140 @@ module alu(
 `include "src\MiniRISC_CPU\opcode_defs.vh"
 
 //******************************************************************************
-//* Az aritmetikai mûveletek végrehajtása (összeadás és kivonás).              *
+//* Az aritmetikai mï¿½veletek vï¿½grehajtï¿½sa (ï¿½sszeadï¿½s ï¿½s kivonï¿½s).              *
 //*                                                                            *
-//* Kivonás esetén a 2. operandus kettes komplemensét (/op2 + 1) adjuk hozzá   *
-//* az 1. operandushoz. Összeadó/kivonó logikát nem tudunk itt használni, mert *
-//* szükséges az átvitel bemenet és kimenet is.                                *
+//* Kivonï¿½s esetï¿½n a 2. operandus kettes komplemensï¿½t (/op2 + 1) adjuk hozzï¿½   *
+//* az 1. operandushoz. ï¿½sszeadï¿½/kivonï¿½ logikï¿½t nem tudunk itt hasznï¿½lni, mert *
+//* szï¿½ksï¿½ges az ï¿½tvitel bemenet ï¿½s kimenet is.                                *
 //******************************************************************************
 
-//Az arith_sel[1] bit határozza meg az elvégzendõ mûveletet:
-//0: összeadás
-//1: kivonás (kettes komplemens hozzáadása)
+//Az arith_sel[1] bit hatï¿½rozza meg az elvï¿½gzendï¿½ mï¿½veletet:
+//0: ï¿½sszeadï¿½s
+//1: kivonï¿½s (kettes komplemens hozzï¿½adï¿½sa)
 wire [7:0] adder_op2 = operand2 ^ {8{arith_sel[1]}};
 
-//Az arith_sel[0] bit választja ki a C flag használatát:
-//0: mûveletvégzés átvitel nélkül
-//1: mûveletvégzés átvitellel (kivonás esetén negáljuk)
+//Az arith_sel[0] bit vï¿½lasztja ki a C flag hasznï¿½latï¿½t:
+//0: mï¿½veletvï¿½gzï¿½s ï¿½tvitel nï¿½lkï¿½l
+//1: mï¿½veletvï¿½gzï¿½s ï¿½tvitellel (kivonï¿½s esetï¿½n negï¿½ljuk)
 wire carry_in  = (arith_sel[0]) ? (flag_c ^ arith_sel[1]) : arith_sel[1];
 
-//Az összeadás elvégzése. Az eredmény MSb-je a kimeneti átvitel bit.
+//Az ï¿½sszeadï¿½s elvï¿½gzï¿½se. Az eredmï¿½ny MSb-je a kimeneti ï¿½tvitel bit.
 wire [8:0] sum = operand1 + adder_op2 + carry_in;
 
-//Kivonás esetén a kimenõ átvitel bitet negáljuk.
+//Kivonï¿½s esetï¿½n a kimenï¿½ ï¿½tvitel bitet negï¿½ljuk.
 wire [8:0] arith_result = {sum[8] ^ arith_sel[1], sum[7:0]};
 
-//A kettes komplemens túlcsordulás észlelése. Túlcsordulás akkor történik,
-//ha azonos elõjelû számokat adunk össze és az összeg elõjele ettõl eltér.
+//A kettes komplemens tï¿½lcsordulï¿½s ï¿½szlelï¿½se. Tï¿½lcsordulï¿½s akkor tï¿½rtï¿½nik,
+//ha azonos elï¿½jelï¿½ szï¿½mokat adunk ï¿½ssze ï¿½s az ï¿½sszeg elï¿½jele ettï¿½l eltï¿½r.
 wire overflow = (~operand1[7] & ~adder_op2[7] &  arith_result[7]) |
                 ( operand1[7] &  adder_op2[7] & ~arith_result[7]);
 
 
 //******************************************************************************
-//* A logikai mûveletek végrehajtása (bitenkénti ÉS, VAGY, XOR). Itt hajtjuk   *
-//* végre az alsó és a felsõ 4 bit felcserélését is.                           *
+//* A logikai mï¿½veletek vï¿½grehajtï¿½sa (bitenkï¿½nti ï¿½S, VAGY, XOR). Itt hajtjuk   *
+//* vï¿½gre az alsï¿½ ï¿½s a felsï¿½ 4 bit felcserï¿½lï¿½sï¿½t is.                           *
 //******************************************************************************
 reg [7:0] logic_result;
 
 always @(*)
 begin
    case (logic_sel)
-      //Bitenkénti ÉS
+      //Bitenkï¿½nti ï¿½S
       LOGIC_AND: logic_result <= operand1 & operand2;
-      //Bitenkénti VAGY
+      //Bitenkï¿½nti VAGY
       LOGIC_OR : logic_result <= operand1 | operand2;
-      //Bitenkénti XOR
+      //Bitenkï¿½nti XOR
       LOGIC_XOR: logic_result <= operand1 ^ operand2;
-      //Az alsó és a felsõ 4 bit felcserélése
+      //Az alsï¿½ ï¿½s a felsï¿½ 4 bit felcserï¿½lï¿½se
       default  : logic_result <= {operand1[3:0], operand1[7:4]};
    endcase
 end
 
 
 //******************************************************************************
-//* A shiftelési és a forgatási mûveletek elvégzése.                           *
+//* A shiftelï¿½si ï¿½s a forgatï¿½si mï¿½veletek elvï¿½gzï¿½se.                           *
 //******************************************************************************
 reg [8:0] shift_result;
 
-//Forgatás esetén a shift_sel[2] választja ki a beléptetendõ bitet:
-//0: sima forgatás, a kilépõ bit kerül beléptetésre a másik oldalon
-//1: forgatás a carry flag-en keresztül
+//Forgatï¿½s esetï¿½n a shift_sel[2] vï¿½lasztja ki a belï¿½ptetendï¿½ bitet:
+//0: sima forgatï¿½s, a kilï¿½pï¿½ bit kerï¿½l belï¿½ptetï¿½sre a mï¿½sik oldalon
+//1: forgatï¿½s a carry flag-en keresztï¿½l
 wire rot_in_l = (shift_sel[2]) ? flag_c : operand1[7];
 wire rot_in_r = (shift_sel[2]) ? flag_c : operand1[0];
 
-//A shift_sel[3] választja ki a shiftelés típusát:
-//0: normál shiftelés
-//1: aritmetikai shiftelés, az MSb (elõjel bit) helyben marad
+//A shift_sel[3] vï¿½lasztja ki a shiftelï¿½s tï¿½pusï¿½t:
+//0: normï¿½l shiftelï¿½s
+//1: aritmetikai shiftelï¿½s, az MSb (elï¿½jel bit) helyben marad
 wire shr_msb  = (shift_sel[3]) ? operand1[7] : shift_sel[2];
 
-//A shiftelés/forgatás elvégzése. Az eredmény MSb-je a kimeneti átvitel bit.
+//A shiftelï¿½s/forgatï¿½s elvï¿½gzï¿½se. Az eredmï¿½ny MSb-je a kimeneti ï¿½tvitel bit.
 always @(*)
 begin
    case (shift_sel[1:0])
-      //Shiftelés balra
+      //Shiftelï¿½s balra
       SHIFT_SHL: shift_result <= {operand1[7], operand1[6:0], shift_sel[2]};
-      //Shiftelés jobbra
+      //Shiftelï¿½s jobbra
       SHIFT_SHR: shift_result <= {operand1[0], shr_msb, operand1[7:1]};
-      //Forgatás balra
+      //Forgatï¿½s balra
       SHIFT_ROL: shift_result <= {operand1[7], operand1[6:0], rot_in_l};
-      //Forgatás jobbra
+      //Forgatï¿½s jobbra
       SHIFT_ROR: shift_result <= {operand1[0], rot_in_r, operand1[7:1]};
    endcase
 end
 
 
 //******************************************************************************
-//* Az ALU eredményt kiválasztó multiplexer.                                   *
+//* Az ALU eredmï¿½nyt kivï¿½lasztï¿½ multiplexer.                                   *
 //******************************************************************************
 reg [8:0] alu_result;
 
 always @(*)
 begin
    case (op_type)
-      ALU_MOVE : alu_result <= {1'b0, operand2};         //Adatmozgatás
-      ALU_ARITH: alu_result <= arith_result;             //Aritmetikai mûvelet
-      ALU_LOGIC: alu_result <= {1'b0, logic_result};     //Logikai mûvelet
-      ALU_SHIFT: alu_result <= shift_result;             //Shiftelés/forgatás
+      ALU_MOVE : alu_result <= {1'b0, operand2};         //Adatmozgatï¿½s
+      ALU_ARITH: alu_result <= arith_result;             //Aritmetikai mï¿½velet
+      ALU_LOGIC: alu_result <= {1'b0, logic_result};     //Logikai mï¿½velet
+      ALU_SHIFT: alu_result <= shift_result;             //Shiftelï¿½s/forgatï¿½s
    endcase
 end
 
-//A multiplexer kimenetének alsó 8 bitje az ALU eredmény, az MSb
-//a carry flag új értékét tartalmazza.
+//A multiplexer kimenetï¿½nek alsï¿½ 8 bitje az ALU eredmï¿½ny, az MSb
+//a carry flag ï¿½j ï¿½rtï¿½kï¿½t tartalmazza.
 assign result = alu_result[7:0];
 
 
 //******************************************************************************
 //* Zero flag (Z).                                                             *
 //*                                                                            *
-//* Jelzi, ha az ALU mûvelet eredménye 0. Értékét frissíteni kell aritmetikai, *
-//* logikai vagy shiftelési/forgatási mûveletek végrehajtása esetén. Írhatónak *
-//* kell lennie, mert megszakításból való visszatérésnél vissza kell állítani  *
-//* a korábbi értékét.                                                         *
+//* Jelzi, ha az ALU mï¿½velet eredmï¿½nye 0. ï¿½rtï¿½kï¿½t frissï¿½teni kell aritmetikai, *
+//* logikai vagy shiftelï¿½si/forgatï¿½si mï¿½veletek vï¿½grehajtï¿½sa esetï¿½n. ï¿½rhatï¿½nak *
+//* kell lennie, mert megszakï¿½tï¿½sbï¿½l valï¿½ visszatï¿½rï¿½snï¿½l vissza kell ï¿½llï¿½tani  *
+//* a korï¿½bbi ï¿½rtï¿½kï¿½t.                                                         *
 //******************************************************************************
 always @(posedge clk)
 begin
    if (flag_wr)
       flag_z <= flag_din[Z_FLAG];
-   else
+   else begin
       if (op_type != ALU_MOVE)
-         flag_z <= (result == 8'd0);
+      begin
+         // Zero flag akkumulalasa SBC es CMY muveletek eseten
+         if(arith_sel[1] & arith_sel[0])
+            flag_z <= (flag_z) && (result == 8'd0);
+         else
+            flag_z <= (result == 8'd0);
+      end
+   end
 end
 
 
 //******************************************************************************
 //* Carry flag (C).                                                            *
 //*                                                                            *
-//* Jelzi, ha az aritmetikai mûveletek során átvitel történt, illetve felveszi *
-//* a kishiftelt bit értékét shiftelés/forgatás végrehajtása esetén. Írhatónak *
-//* kell lennie, mert megszakításból való visszatérésnél vissza kell állítani  *
-//* a korábbi értékét.                                                         *
+//* Jelzi, ha az aritmetikai mï¿½veletek sorï¿½n ï¿½tvitel tï¿½rtï¿½nt, illetve felveszi *
+//* a kishiftelt bit ï¿½rtï¿½kï¿½t shiftelï¿½s/forgatï¿½s vï¿½grehajtï¿½sa esetï¿½n. ï¿½rhatï¿½nak *
+//* kell lennie, mert megszakï¿½tï¿½sbï¿½l valï¿½ visszatï¿½rï¿½snï¿½l vissza kell ï¿½llï¿½tani  *
+//* a korï¿½bbi ï¿½rtï¿½kï¿½t.                                                         *
 //******************************************************************************
 always @(posedge clk)
 begin
@@ -178,9 +185,9 @@ end
 //******************************************************************************
 //* Negative flag (N).                                                         *
 //*                                                                            *
-//* Jelzi, ha az ALU mûvelet eredménye negatív (az elõjel bit 1). Írhatónak    *
-//* kell lennie, mert megszakításból való visszatérésnél vissza kell állítani  *
-//* a korábbi értékét.                                                         *
+//* Jelzi, ha az ALU mï¿½velet eredmï¿½nye negatï¿½v (az elï¿½jel bit 1). ï¿½rhatï¿½nak    *
+//* kell lennie, mert megszakï¿½tï¿½sbï¿½l valï¿½ visszatï¿½rï¿½snï¿½l vissza kell ï¿½llï¿½tani  *
+//* a korï¿½bbi ï¿½rtï¿½kï¿½t.                                                         *
 //******************************************************************************
 always @(posedge clk)
 begin
@@ -195,9 +202,9 @@ end
 //******************************************************************************
 //* Overflow flag (V).                                                         *
 //*                                                                            *
-//* Jelzi, ha kettes komplemens túlcsordulás történt az aritmetikai mûveletek  *
-//* végrehajtása esetén. Írhatónak kell lennie, mert megszakításból való       *
-//* visszatérésnél vissza kell állítani a korábbi értékét.                     *
+//* Jelzi, ha kettes komplemens tï¿½lcsordulï¿½s tï¿½rtï¿½nt az aritmetikai mï¿½veletek  *
+//* vï¿½grehajtï¿½sa esetï¿½n. ï¿½rhatï¿½nak kell lennie, mert megszakï¿½tï¿½sbï¿½l valï¿½       *
+//* visszatï¿½rï¿½snï¿½l vissza kell ï¿½llï¿½tani a korï¿½bbi ï¿½rtï¿½kï¿½t.                     *
 //******************************************************************************
 always @(posedge clk)
 begin
